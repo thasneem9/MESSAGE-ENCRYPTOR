296ae5cd6c755980a979f26537d46aa6ba6136d1
thasneem9 <135808068+thasneem9@users.noreply.github.com>
2024-04-26 16:00:59 +0530

    Create main.py
    Code contains the message box, buttons.

---
+++ main.py
@@ -0,0 +1,48 @@
+from tkinter import *
+from tkinter import messagebox
+import base64
+import os
+from tkinter import Tk
+from tkinter import PhotoImage
+
+
+def main_screen():
+    global screen
+    global code
+    global text1
+
+
+    screen=Tk()
+    screen.geometry("375x398")
+    #icon
+    image_icon=PhotoImage(file="keys.png")
+    screen.iconphoto(False, image_icon)
+    #app name
+    screen.title("pctApp")
+    #prompt
+    Label(text="Enter text for encryption/decryption", fg="black",font=("calbri",13)).place(x=10,y=10)
+    #the message font
+    text1=Text(font="Robote 20",bg="white", relief=GROOVE,wrap=WORD,bd=0)
+    #textbox 
+    text1.place(x=10,y=50,width=355,height=100)
+  
+    #PASSWORD--------->>>>>>>>>
+
+    Label(text="Enter secret key: ",fg="black",font=("calibri",13)).place(x=10,y=170)
+    code=StringVar()
+    Entry(textvariable=code,width=19,bd=0,font=("arial",25),show="*").place(x=10,y=200)
+
+
+    #ERESET FUNCTION>>>>>>:
+    def reset():
+        code.set("")
+        text1.delete(1.0,END)
+
+    #BUTONS---------->>>>>>>
+    Button(text="ENCRYPT",height="2",width=23,bg="#ed3833",fg="white",bd=0,command=encrypt).place(x=10,y=250)
+    Button(text="DECRYPT",height="2",width=23,bg="#00bd56",fg="white",bd=0,command=decrypt).place(x=200,y=250)
+    Button(text="RESET",height="2",width=50,bg="#1089ff",fg="white",bd=0,command=reset).place(x=10,y=300)
+    screen.mainloop()
+
+
+main_screen()
